fccdfbe8c206c667049493d7d7b98e8cbb69f4f8
Thai Nguyen <privat_thai_nguyen@hotmail.com>
2024-04-07 10:30:59 +0300

    Added even more unit tests

--- src/__tests__/components/utils/section_container_test.tsx
+++ src/__tests__/components/utils/section_container_test.tsx
@@ -1,40 +1,16 @@
-import { render, screen, within, fireEvent } from "@testing-library/react";
+/*import { render, screen, within, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom'
 import randomNumber from "../../../tools/random_number";
 import PopupMessage from "../../../components/utils/popup_message";
 
 
-const mockText = randomNumber().toString();
-const mockTitle = randomNumber().toString();
-const pButton = {
-    text: randomNumber().toString(),
-    callback: jest.fn()
-}
-const sButton = {
-    text: randomNumber().toString(),
-    callback: jest.fn()
-}
 
 describe("Test <SectionContainer>", () => {
-    test("Renders ok and buttons work", () => {
+    test("Renders ok and can expand", () => {
         render(
-            <PopupMessage title={mockTitle} text={mockText} primaryButton={pButton} secondaryButton={sButton} />
+            
         )
 
-        const heading = screen.getByRole("heading");
-        expect(heading).toHaveTextContent(mockTitle);
-
-        const text = screen.getByText(mockText);
-        expect(text).toBeVisible();
-        
-        const primaryButton = screen.getByText(pButton.text, { selector: "button" });
-        expect(primaryButton).toBeVisible();
-        fireEvent.click(primaryButton);
-        expect(pButton.callback).toHaveBeenCalled();
-
-        const secondaryButton = screen.getByText(sButton.text, { selector: "button" });
-        expect(secondaryButton).toBeVisible();
-        fireEvent.click(secondaryButton);
-        expect(sButton.callback).toHaveBeenCalled();
+   
     })
-});
+});*/
