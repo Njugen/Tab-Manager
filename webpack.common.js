e240749de48c21697d774bd082dce11e3ad49415
Thai Nguyen <privat_thai_nguyen@hotmail.com>
2024-04-28 19:44:44 +0300

    Added env to webpack. building for firefox and chrome can now be done using cmd

--- webpack.common.js
+++ webpack.common.js
@@ -4,61 +4,90 @@ const HtmlPlugin = require("html-webpack-plugin");
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 
-module.exports = {
-    entry: {
-        options: path.resolve(__dirname, "src/index.tsx"), // Top react component for option's page.
-        sidepanel: path.resolve(__dirname, "src/sidepanel.tsx"), // Top react component for sidepanel
-        background: path.resolve(__dirname, "src/webextension/background/background.ts"), // Script running in the browser's internal environment
-        contentScript: path.resolve(__dirname, "src/webextension/contentScript/contentScript.ts"), // Script running in the plugin's UI (option's page, sidepanels, controller)
-    },
-    module: {
-        rules: [
-            { use: "ts-loader", test: /\.tsx?$/, exclude: /node_modules/ }, 
-            { test: /\.scss$|css$/, use: [ 
-               "style-loader", "css-loader",
-                { 
-                    loader: "postcss-loader", 
-                    options: {
-                        postcssOptions: {
-                            ident: "postcss",
-                            plugins: [tailwindcss, autoprefixer]
-                        }
-                    }
-                }, 
-            ]}, 
+module.exports = (env) => {
+    const distFolder = () => {
+        // Return name of the dist folders depending on what browser the plugin is being built for
+        let name = "dist-chrome";
 
-        ]
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { 
-                    from: path.resolve(__dirname, "src/webextension/manifest.json"), 
-                    to: path.resolve("dist")
-                },
-                { 
-                    from: path.resolve(__dirname, "brand"), 
-                    to: path.resolve(__dirname, "dist/brand")
-                }
-            ]
-        }),
-        new HtmlPlugin({
-            title: "Sidepanel",
-            filename: "sidepanel.html",
-            chunks: ["sidepanel"]
-        }), 
-        new HtmlPlugin({
-            title: "Tab Manager",
-            filename: "options.html",
-            chunks: ["options"]
-        }),
-        
-    ],
-    resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
-    },
-    output: {
-        filename: "[name].js"  // in /dist
+        if(env.browser === "firefox"){
+            name = "dist-firefox";
+        }
+
+        return name;
     }
+
+    const originManifest = () => {
+        // Return path to manifest.
+        // Different browsers might use different manifest formats.
+        // Return path to chromium manifest per default.
+
+        let path = "src/webextension";
+
+        if(env.browser === "firefox"){
+            return `${path}/manifest_firefox.json`;
+        }
+
+        return `${path}/manifest_chrome.json`;
+    }
+
+    return {
+        entry: {
+            options: path.resolve(__dirname, "src/index.tsx"), // Top react component for option's page.
+            sidepanel: path.resolve(__dirname, "src/sidepanel.tsx"), // Top react component for sidepanel
+            background: path.resolve(__dirname, "src/webextension/background/background.ts"), // Script running in the browser's internal environment
+            contentScript: path.resolve(__dirname, "src/webextension/contentScript/contentScript.ts"), // Script running in the plugin's UI (option's page, sidepanels, controller)
+        },
+        module: {
+            rules: [
+                { use: "ts-loader", test: /\.tsx?$/, exclude: /node_modules/ }, 
+                { test: /\.scss$|css$/, use: [ 
+                "style-loader", "css-loader",
+                    { 
+                        loader: "postcss-loader", 
+                        options: {
+                            postcssOptions: {
+                                ident: "postcss",
+                                plugins: [tailwindcss, autoprefixer]
+                            }
+                        }
+                    }, 
+                ]}, 
+
+            ]
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { 
+                        from: path.resolve(__dirname, originManifest()), 
+                        to: path.resolve(`${distFolder()}/manifest.json`)
+                    },
+                    { 
+                        from: path.resolve(__dirname, "brand"), 
+                        to: path.resolve(__dirname, `${distFolder()}/brand`)
+                    }
+                ]
+            }),
+            new HtmlPlugin({
+                title: "Sidepanel",
+                filename: "sidepanel.html",
+                chunks: ["sidepanel"]
+            }), 
+            new HtmlPlugin({
+                title: "Tab Manager",
+                filename: "options.html",
+                chunks: ["options"]
+            }),
+            
+        ],
+        resolve: {
+            // Add `.ts` and `.tsx` as a resolvable extension.
+            extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
+        },
+        output: {
+            filename: "[name].js",  // in /dist
+            path: path.join(__dirname, distFolder())
+        }
+    }
+    
 }; 
